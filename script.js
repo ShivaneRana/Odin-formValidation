785fbdea43d27f9f733dfa2cedcbca62fac7df99
Shivane Rana <shivanerana2004@gmail.com>
2024-07-27 15:57:58 +0530

    radomImage functino created to update random image in side panel

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const image = document.querySelector(".image");
+const image = document.querySelector(".imagePanel");
 const content = document.querySelector(".content");
+
+//this apply random image to the sidebar
+function randomImage(){
+    const randomN = Math.floor((Math.random()*8)+1);
+    image.style.backgroundImage = `url("./image/${randomN}.jpg")`;
+    console.log(`Random image number ${randomN} has been applied`);
+}
+randomImage();
